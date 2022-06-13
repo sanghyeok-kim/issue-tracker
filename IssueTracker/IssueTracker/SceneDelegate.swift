f9f4efd77ea72042c552b542cae4606df1b3db09
sanghyeok-kim <slsnsi1212@gmail.com>
2022-06-13 17:39:54 +0900

    feat: Github OAuth 로그인 작업 (access_token까지 받아옴)
    rising-jun/issue-tracker/#2

--- IssueTracker/IssueTracker/SceneDelegate.swift
+++ IssueTracker/IssueTracker/SceneDelegate.swift
@@ -23,23 +23,37 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let tempCode = url?.absoluteString.components(separatedBy: "code=").last ?? ""
         
         let clientID = "2ca00a62da0566df46d7"
-        
+        let clientSecret = "bf9ac50f855cabc69474299b177fa9e5082bdf07"
         let urlString = "https://github.com/login/oauth/access_token"
         guard var components = URLComponents(string: urlString) else { return }
         components.queryItems = [
             URLQueryItem(name: "client_id", value: clientID),
-            URLQueryItem(name: "client_secret", value: clientID),
+            URLQueryItem(name: "client_secret", value: clientSecret),
             URLQueryItem(name: "code", value: tempCode)
         ]
         
         guard let url = components.url else { return }
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
         
         let session = URLSession(configuration: .default)
         session.dataTask(with: request) { data, response, error in
-            print(String(data: data ?? Data(), encoding: .utf8))
+            
+            guard let data = data else { return }
+            do {
+                let decodedData = try JSONDecoder().decode(Token.self, from: data)
+                print(decodedData.access_token)
+            } catch {
+                print(error)
+            }
         }.resume()
-        
     }
+}
+
+struct Token: Decodable {
+    let access_token: String
+    let scope: String
+    let token_type: String
 }
